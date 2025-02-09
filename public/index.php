9338a5a3679486d1d9936c9c06be8b6fe9a83856
Nam <namcaodev@gmail.com>
2025-02-09 10:25:44 +0700

    chore: add init git

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1); // public/index.php
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+dd('namdepzai');
+
+// request received
+
+// perform some logic
+
+// send response (string of content)
+echo 'Hello World';
